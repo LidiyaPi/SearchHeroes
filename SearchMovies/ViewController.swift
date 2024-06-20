26f5b002c7eaab22a88be43c905668725ff00549
Лидия Пятаева <z_liya@inbox.ru>
2024-06-20 21:07:52 +0500

    Initial Commit

---
+++ SearchMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchMovies
+//
+//  Created by Лидия Пятаева on 20.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
