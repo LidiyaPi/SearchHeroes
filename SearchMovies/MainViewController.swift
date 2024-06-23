c82516075fb32db7a176bf231c34eb801815d502
Лидия Пятаева <z_liya@inbox.ru>
2024-06-23 16:38:07 +0500

    collectionView

--- SearchMovies/MainViewController.swift
+++ SearchMovies/MainViewController.swift
@@ -9,12 +9,39 @@ import UIKit
 import Foundation
 
 
-class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MainViewController: UICollectionViewController {
+    
+ 
+    
+   
+    
    
     override func viewDidLoad() {
         super.viewDidLoad()
+
       
     }
-
-   
+ 
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "movieCell", for: indexPath)
+        guard let cell = cell as? MovieCollectionViewCell else { return UICollectionViewCell()}
+        return cell
+    }
+    
+  
 }
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        CGSize(width: UIScreen.main.bounds.width - 48, height: 100)
+    }
+}
+
+
